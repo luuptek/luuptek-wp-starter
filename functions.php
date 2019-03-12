b4f205ade740a76023c5fb008df59fd1316aab50
Timo Leppänen <timo@luuptek.fi>
2019-03-12 11:37:49 +0200

    some comment removing

--- functions.php
+++ functions.php
@@ -111,7 +111,6 @@ if ( ! function_exists( 'luuptek_wp_base_setup' ) ) :
             add_theme_support( 'html5', [ 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ] );
             add_theme_support( 'post-thumbnails' );
             add_theme_support( 'title-tag' );
-            //add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
         }
 
         /**
