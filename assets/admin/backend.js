0171fafb39c4a1984e8e678c2ce6d145cdf86e0b
Timo Leppänen <timo@luuptek.fi>
2019-04-15 12:12:10 +0300

    small improvements

--- assets/admin/backend.js
+++ assets/admin/backend.js
@@ -1,14 +1,8 @@
 wp.domReady( () => {
-    wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
-    wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-    wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
 
-    wp.blocks.registerBlockStyle( 'core/button', {
-        name: 'default',
-        label: 'Default',
-        isDefault: true,
-    });
-
+    /**
+     * Add full width style button
+     */
     wp.blocks.registerBlockStyle( 'core/button', {
         name: 'full-width',
         label: 'Full Width',
