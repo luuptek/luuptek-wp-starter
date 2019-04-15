42e002beafba29cbdb7b069cbf6c03db5cd8f0cc
Timo Leppänen <timo@luuptek.fi>
2019-04-15 20:25:33 +0300

    create ready made footer widgets

--- library/widgets/widgets.php
+++ library/widgets/widgets.php
@@ -11,15 +11,22 @@
  * Register widgetized areas
  */
 add_action( 'widgets_init', function () {
-    $widget_base = [
-        'before_widget' => '<section class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h3 class="widget__title">',
-        'after_title'   => '</h3>'
-    ];
+	$footer_widget_base = [
+		'before_widget' => '<div class="footer-column widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h3 class="widget__title">',
+		'after_title'   => '</h3>'
+	];
 
-    register_sidebar( [
-        'name' => __( 'Sidebar', TEXT_DOMAIN ),
-        'id' => 'sidebar-main'
-    ] + $widget_base );
+	register_sidebar(
+		[
+			'name' => __( 'Footer 1/2', TEXT_DOMAIN ),
+			'id'   => 'footer-1'
+		] + $footer_widget_base );
+
+	register_sidebar(
+		[
+			'name' => __( 'Footer 2/2', TEXT_DOMAIN ),
+			'id'   => 'footer-2'
+		] + $footer_widget_base );
 } );
--- templates/footer.php
+++ templates/footer.php
@@ -8,6 +8,23 @@
 
 ?>
 
+<footer>
+    <div class="container">
+        <div class="row">
+			<?php
+
+			$footer_widgets = [ 'footer-1', 'footer-2' ];
+
+			foreach ( $footer_widgets as $footer_widget ) {
+				if ( is_active_sidebar( $footer_widget ) ) {
+					dynamic_sidebar( $footer_widget );
+				}
+			}
+			?>
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
