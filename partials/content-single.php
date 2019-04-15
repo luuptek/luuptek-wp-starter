8b45a3a2c370047ba64ec613db3708a185082171
Timo Leppänen <timo@luuptek.fi>
2019-04-15 18:55:30 +0300

    add needed class to single template to support gutenberg

--- partials/content-single.php
+++ partials/content-single.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article <?php post_class( 'post-container post-' . sanitize_title( get_the_title() ) ); ?>>
+<article <?php post_class( 'post-container gutenberg post-' . sanitize_title( get_the_title() ) ); ?>>
 	<h1><?php the_title(); ?></h1>
 	<?php the_content(); ?>
 </article>
