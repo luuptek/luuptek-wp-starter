ccc174e8bd29e85d6e00a98b2b38ef97117d1cbc
Timo Leppänen <timo@luuptek.fi>
2019-05-26 13:46:47 +0300

    single.php content inside container

--- templates/single.php
+++ templates/single.php
@@ -13,14 +13,12 @@ get_header();
 <?php do_action( 'luuptek_wp_base_before_page' ); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<?php get_template_part( 'partials/content', get_post_type() ); ?>
+    <section>
+        <div class="container">
+			<?php get_template_part( 'partials/content', get_post_type() ); ?>
+        </div>
+    </section>
 <?php endwhile; endif; ?>
-
-<?php
-if ( comments_open() || '0' != get_comments_number() ) :
-	comments_template();
-endif;
-?>
 
 <?php do_action( 'luuptek_wp_base_after_page' ); ?>
 
