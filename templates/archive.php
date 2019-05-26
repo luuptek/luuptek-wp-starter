b1582621ed8d2eb4bee8a6f9bfb51e6e6754c97a
Timo Leppänen <timo@luuptek.fi>
2019-05-26 13:36:56 +0300

    archive content inside container

--- templates/archive.php
+++ templates/archive.php
@@ -12,16 +12,20 @@ get_header();
 
 <?php do_action( 'luuptek_wp_base_before_page' ); ?>
 
-	<section>
-		<h1><?php echo get_the_archive_title(); ?></h1>
+<section>
+    <div class="container">
+        <h1><?php echo get_the_archive_title(); ?></h1>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'partials/content', 'excerpt' ); ?>
 		<?php endwhile; endif; ?>
-	</section>
+    </div>
+</section>
 
-	<section class="pagination">
+<section class="pagination">
+    <div class="container">
 		<?php echo UTILS()->pagination(); ?>
-	</section>
+    </div>
+</section>
 
 <?php do_action( 'luuptek_wp_base_after_page' ); ?>
 
--- templates/index.php
+++ templates/index.php
@@ -13,19 +13,23 @@ get_header();
 <?php do_action( 'luuptek_wp_base_before_page' ); ?>
 
 <section>
-	<h1><?php echo get_the_archive_title(); ?></h1>
-	<?php
-	if ( have_posts() ) : while ( have_posts() ) : the_post();
-		get_template_part( 'partials/content', 'excerpt' );
-	endwhile;
-	else :
-		get_template_part( 'partials/no-results' );
-	endif;
-	?>
+    <div class="container">
+        <h1><?php echo get_the_archive_title(); ?></h1>
+		<?php
+		if ( have_posts() ) : while ( have_posts() ) : the_post();
+			get_template_part( 'partials/content', 'excerpt' );
+		endwhile;
+		else :
+			get_template_part( 'partials/no-results' );
+		endif;
+		?>
+    </div>
 </section>
 
 <section class="pagination">
-	<?php echo UTILS()->pagination(); ?>
+    <div class="container">
+		<?php echo UTILS()->pagination(); ?>
+    </div>
 </section>
 
 <?php do_action( 'luuptek_wp_base_after_page' ); ?>
