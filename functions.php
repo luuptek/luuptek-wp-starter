1549dc67d371c0c2377ad6959a5a1f46def4bac9
Timo Leppänen <timo@luuptek.fi>
2019-12-10 19:56:58 +0200

    Remove custom color picker

--- functions.php
+++ functions.php
@@ -144,6 +144,7 @@ if ( ! function_exists( 'luuptek_wp_base_setup' ) ) :
 
 			add_theme_support( 'align-wide' );
 			add_theme_support( 'responsive-embeds' );
+			add_theme_support( 'disable-custom-colors' );
 		}
 
 		/**
