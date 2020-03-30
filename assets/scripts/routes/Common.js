aef0ccdfc59f0961dc3ca237172889d9ab42f7d1
Timo Leppänen <timo@luuptek.fi>
2020-03-30 11:18:57 +0300

    hamburger as part of mobile menu, multiple options available

--- assets/scripts/routes/Common.js
+++ assets/scripts/routes/Common.js
@@ -1,6 +1,10 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+
+      $('#mainNav').on('show.bs.collapse hide.bs.collapse', () => {
+          $('button.hamburger').toggleClass('is-active');
+      });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
--- partials/components/main-nav.php
+++ partials/components/main-nav.php
@@ -3,10 +3,11 @@
         <a class="navbar-brand" href="<?php echo home_url( '/' ); ?>">
             <img src="<?php echo UTILS()->get_image_uri() . '/logo.svg' ?>" class="nav-logo" alt="Logo"/>
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav"
-                aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="<?php _e( 'Toggle navigation', TEXT_DOMAIN ); ?>">
-            <i class="fa fa-bars" aria-hidden="true"></i>
+        <button class="hamburger hamburger--elastic" type="button" data-toggle="collapse" data-target="#mainNav"
+                aria-label="<?php _e( 'Toggle navigation', TEXT_DOMAIN ); ?>" aria-controls="navigation" aria-expanded="false">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
         </button>
 
         <div class="collapse navbar-collapse" id="mainNav">
