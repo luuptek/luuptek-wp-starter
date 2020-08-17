b5bb475ace2663f8ef8a7f93851442ea7e19948b
Timo Leppänen <timo@luuptek.fi>
2020-08-17 14:50:59 +0300

    fix pagination arrow

--- partials/pagination.php
+++ partials/pagination.php
@@ -2,7 +2,7 @@
 the_posts_pagination(
 	array(
 		'mid_size'  => 3,
-		'prev_text' => __( '<i class="fa fa-angle-double-left" aria-hidden="true"></i>', 'luuptek_wp_base' ),
-		'next_text' => __( '<i class="fa fa-angle-double-right" aria-hidden="true"></i>', 'luuptek_wp_base' ),
+		'prev_text' => '<',
+		'next_text' => '>',
 	)
 );
