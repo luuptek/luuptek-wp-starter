cbe93217319d45666ff189c88b830b74a436b62a
Timo Leppänen <timo@luuptek.fi>
2020-08-26 11:51:59 +0300

    add options for core spacer block to override inline style (which is crap)

--- assets/admin/backend.js
+++ assets/admin/backend.js
@@ -1,11 +1,27 @@
-wp.domReady( () => {
+wp.domReady(() => {
 
-    /**
-     * Add full width style button
-     */
-    wp.blocks.registerBlockStyle( 'core/button', {
-        name: 'full-width',
-        label: 'Full Width',
-    } );
+	/**
+	 * Add full width style button
+	 */
+	wp.blocks.registerBlockStyle('core/button', {
+		name: 'full-width',
+		label: 'Full Width',
+	});
 
-} );
+	wp.blocks.registerBlockStyle('core/spacer', {
+		name: 'responsive-large',
+		label: 'Large',
+	});
+
+	wp.blocks.registerBlockStyle('core/spacer', {
+		name: 'responsive-medium',
+		label: 'Medium',
+		isDefault: true
+	});
+
+	wp.blocks.registerBlockStyle('core/spacer', {
+		name: 'responsive-small',
+		label: 'Small',
+	});
+
+});
