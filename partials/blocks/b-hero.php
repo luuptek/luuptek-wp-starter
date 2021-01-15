908433bbd095a98f853e8ba45d93799ce9e381ce
Timo Leppänen <timo@luuptek.fi>
2021-01-15 21:50:19 +0200

    lets check for title in hero

--- partials/blocks/b-hero.php
+++ partials/blocks/b-hero.php
@@ -1,5 +1,17 @@
 <?php
 
+$args = [
+	'image_id'    => get_field( 'hero_bg_image' ),
+	'title'       => get_field( 'hero_title' ),
+	'description' => get_field( 'hero_description' ),
+	'button'      => get_field( 'hero_button' ),
+];
+
+// No render if no title
+if ( empty( $args['title'] ) ) {
+	return;
+}
+
 $class_names = [
 	'b-hero',
 ];
@@ -16,6 +28,7 @@ $args = [
 <div class="<?php echo esc_attr( implode( ' ', $class_names ) ); ?>">
 	<?php
 
+	// Lets read these values here, needed for copy/paste if carousel needed
 	$hero_title  = $args['title'];
 	$description = $args['description'];
 	$image_id    = $args['image_id'];
