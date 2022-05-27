7bf01a5551c06db1f90dd1f9db1202d7cd6f2dfb
Timo Leppänen <timo@luuptek.fi>
2022-05-27 13:03:13 +0300

    lang switcher to work out of the box

--- partials/components/lang-selector.php
+++ partials/components/lang-selector.php
@@ -7,22 +7,30 @@ if ( ! function_exists( 'pll_current_language' ) ) {
 
 $translations = pll_the_languages( array( 'raw' => 1 ) );
 
+if ( count( $translations ) === 0 ) {
+	return;
+}
+
 foreach ( $translations as $translation ) {
 	if ( pll_current_language() === $translation['slug'] ) {
 		?>
-		<li class="nav-item menu-item-has-children dropdown">
-			<a title="Palvelut" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"
-			   class="dropdown-toggle nav-link nav-link__lang-link">
-				<?php echo $translation['name'] ?>
+		<li class="menu-item menu-item-has-children dropdown nav-item nav-item--level-1">
+			<a href="#" class="nav-link nav-link--lang-switch nav-link--level-1">
+				<?php echo esc_html( pll_current_language( 'slug' ) ); ?>
+				<button class="main-nav__sub-menu-toggler" aria-haspopup="menu" aria-label="Change the language"
+				        lang="en"
+				        aria-expanded="false">
+					+
+				</button>
 			</a>
-			<ul class="dropdown-menu dropdown-menu--navigation">
+			<ul class="main-nav__dropdown main-nav__dropdown-menu--level-1">
 				<?php
 				foreach ( $translations as $translation ) {
 					if ( pll_current_language() !== $translation['slug'] ) {
 						?>
-						<li class="nav-item">
+						<li class="menu-item nav-item nav-item--level-2">
 							<a href="<?php echo esc_url( $translation['url'] ); ?>"
-							   class="dropdown-item dropdown-item__lang-item">
+							   lang="<?php echo esc_attr( $translation['slug'] ); ?>" class="dropdown-item--level-2">
 								<?php echo esc_html( $translation['name'] ); ?>
 							</a>
 						</li>
