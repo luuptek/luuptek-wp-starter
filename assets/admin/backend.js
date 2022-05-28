3167d73a1fa88341d38ac5cfd686f7613e55a3c1
Timo Leppänen <timo@luuptek.fi>
2022-05-28 17:50:16 +0300

    remove backend.js gutenberg modifications as it's handled via mu-plugin

--- assets/admin/backend.js
+++ assets/admin/backend.js
@@ -1,24 +1,3 @@
 wp.domReady(() => {
 
-	/**
-	 * Add full width style button
-	 */
-
-	/**
-	 * Get rid of all GB variations
-	 */
-	wp.blocks.unregisterBlockStyle("core/pullquote", [
-		"default",
-		"solid-color"
-	]);
-
-	wp.blocks.unregisterBlockStyle("core/quote", ["default", "large"]);
-
-	wp.blocks.unregisterBlockStyle("core/button", [
-		"default",
-		"outline",
-		"squared",
-		"fill"
-	]);
-
 });
