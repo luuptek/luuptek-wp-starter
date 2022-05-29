cd8ff424aafebf3631e1372537f7139b18273ca1
Timo Leppänen <timo@luuptek.fi>
2022-05-29 13:45:56 +0300

    forms restyled

--- templates/searchform.php
+++ templates/searchform.php
@@ -9,12 +9,15 @@
 ?>
 
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div class="form-group">
-		<label class="show-for-sr" for="s"><?php echo esc_html_x( 'Search for:', 'label' ); ?></label>
-		<input type="text" class="form-control" value="<?php echo get_search_query(); ?>" name="s" id="s"
+	<div class="luuptek-form-field-group">
+		<label class="luuptek-form-label"
+		       for="s"><?php echo esc_html_x( 'Hae:', 'search label', 'luuptek_wp_base' ); ?></label>
+		<input type="text" class="luuptek-form-control" value="<?php echo get_search_query(); ?>" name="s" id="s"
 		       placeholder="<?php esc_html_e( 'Syötä hakusanat', 'luuptek_wp_base' ); ?>"/>
 	</div>
-	<button type="submit" class="btn btn-primary">
-		<?php _e( 'Hae', 'luuptek_wp_base' ); ?>
-	</button>
+	<div class="wp-block-button">
+		<button type="submit" class="wp-block-button__link">
+			<?php _e( 'Hae', 'luuptek_wp_base' ); ?>
+		</button>
+	</div>
 </form>
