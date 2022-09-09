f83311a531fca457f6e50279f7e2d5db5a81fd50
Timo Leppänen <timo@luuptek.fi>
2022-09-09 09:34:51 +0300

    set tabulator order in main nav correctly

--- partials/components/main-nav.php
+++ partials/components/main-nav.php
@@ -5,22 +5,17 @@
 			<img src="<?php echo esc_url( UTILS()->get_image_uri() . '/logo.svg' ); ?>" class="main-nav__nav-logo"
 			     alt="Logo"/>
 		</a>
-		<div class="main-nav__actions">
-			<button class="main-nav__search open-main-search" type="button"
-			        aria-label="<?php esc_html_e( 'Avaa haku', 'luuptek_wp_base' ); ?>">
-				<?php Utils()->the_svg( 'icons/search' ); ?>
-			</button>
-			<button class="main-nav__mobile-menu-toggler hamburger hamburger--elastic" type="button"
-			        data-toggle="collapse"
-			        data-target="#mainNav"
-			        aria-label="<?php esc_attr( pll__( 'Mobiilivalikon avaaja ja sulkija' ) ); ?>"
-			        aria-controls="mainNav"
-			        aria-expanded="false">
+
+		<button class="main-nav__mobile-menu-toggler hamburger hamburger--elastic" type="button"
+		        data-toggle="collapse"
+		        data-target="#mainNav"
+		        aria-label="<?php esc_attr( pll__( 'Mobiilivalikon avaaja ja sulkija' ) ); ?>"
+		        aria-controls="mainNav"
+		        aria-expanded="false">
             <span class="hamburger-box">
               <span class="hamburger-inner"></span>
             </span>
-			</button>
-		</div>
+		</button>
 
 		<div class="main-nav__main-menu-wrapper collapse" id="mainNav">
 			<ul class="main-nav__main-menu">
@@ -28,5 +23,9 @@
 				<?php get_template_part( 'partials/components/lang-selector' ); ?>
 			</ul>
 		</div>
+		<button class="main-nav__search open-main-search" type="button"
+		        aria-label="<?php esc_html_e( 'Avaa haku', 'luuptek_wp_base' ); ?>">
+			<?php Utils()->the_svg( 'icons/search' ); ?>
+		</button>
 	</div>
 </nav>
