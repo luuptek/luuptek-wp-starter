7d2ccc21d854529b4755ca5fb9e3d1f5cf6c2d89
Timo Leppänen <timo@luuptek.fi>
2023-05-17 17:30:01 +0300

    this not needed anymore

--- library/widgets/nav-menus.php
+++ library/widgets/nav-menus.php
@@ -7,31 +7,6 @@
  */
 
 /**
- * Main menu
- */
-function luuptek_wp_base_main_menu() {
-	wp_nav_menu(
-		[
-			'theme_location'  => 'top_nav',
-			'container'       => false,
-			'container_class' => '',
-			'container_id'    => '',
-			'menu_class'      => 'menu',
-			'menu_id'         => '',
-			'echo'            => true,
-			'fallback_cb'     => '',
-			'before'          => '',
-			'after'           => '',
-			'link_before'     => '',
-			'link_after'      => '',
-			'items_wrap'      => '%3$s',
-			'depth'           => 4,
-			'walker'          => new WP_Bootstrap_Navwalker(),
-		]
-	);
-}
-
-/**
  * Register the menu
  */
 register_nav_menus(
