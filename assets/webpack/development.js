ba545892f821ec952d81107c364b6619b7ec6335
Timo Leppänen <timo@luuptek.fi>
2023-09-04 11:37:55 +0300

    upgrade node version, also create dev file when dev is ongoing

--- assets/webpack/development.js
+++ assets/webpack/development.js
@@ -1,9 +1,24 @@
 const { merge } = require('webpack-merge');
+const fs = require('fs');
 
 const webpackBase = require('./webpack.base.js');
 const plugins = require('./plugins');
 
 module.exports = merge(webpackBase, {
-  devtool: 'eval',
-  plugins: plugins.develop
+    devtool: 'eval',
+    plugins: [
+        ...plugins.develop,
+        {
+            apply: (compiler) => {
+                compiler.hooks.beforeCompile.tapAsync('CreateDevOngoingFilePlugin', (params, callback) => {
+                    fs.writeFile('.DEV-ONGOING', '', (err) => {
+                        if (err) throw err;
+                        console.log('.DEV-ONGOING file has been created.');
+                        callback();
+                    });
+                });
+            }
+        }
+    ]
 });
+
--- assets/webpack/production.js
+++ assets/webpack/production.js
@@ -1,12 +1,31 @@
 const { merge } = require('webpack-merge');
+const fs = require('fs');
 const webpackBase = require('./webpack.base.js');
 const plugins = require('./plugins');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(webpackBase, {
-  plugins: plugins.production,
-	optimization: {
-		minimize: true,
-		minimizer: [new TerserPlugin()],
-	},
+    plugins: [
+        ...plugins.production,
+        {
+            apply: (compiler) => {
+                compiler.hooks.beforeCompile.tapAsync('DeleteDevOngoingFilePlugin', (params, callback) => {
+                    if (fs.existsSync('.DEV-ONGOING')) {
+                        fs.unlink('.DEV-ONGOING', (err) => {
+                            if (err) throw err;
+                            console.log('.DEV-ONGOING file has been deleted.');
+                            callback();
+                        });
+                    } else {
+                        callback();
+                    }
+                });
+            }
+        }
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+    }
 });
+
